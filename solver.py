b3e46a1244f2c4953c47e6429222095d66354266
SEU-ChangYunhan <yunhan_chang@berkeley.edu>
2025-05-08 21:22:36 -0700

    finish the general search

--- solver.py
+++ solver.py
@@ -1,10 +1,10 @@
 import heapq,time
 
-def general_search(initial_state, heuristic_func=None):
+def general_search(initial_state, heuristic=None):
 
     # uniform search uses 0 h(n)
-    if heuristic_func is None:
-        heuristic_func = lambda s: 0
+    if heuristic is None:
+        heuristic = lambda s: 0
 
     # initialize stat
     nodes_expanded = 0
@@ -12,7 +12,7 @@ def general_search(initial_state, heuristic_func=None):
     start_time = time.time()
 
     # initialize priority queue
-    start_f = initial_state.cost + heuristic_func(initial_state)
+    start_f = initial_state.cost + heuristic(initial_state)
     frontier = [(start_f, 0, initial_state)]
     heapq.heapify(frontier)
 
@@ -23,3 +23,58 @@ def general_search(initial_state, heuristic_func=None):
     #dictionary to check states faster than p
     counter = 1
     #used in even f(n)
+
+    while frontier:
+        # Update maximum queue size
+        max_queue_size = max(max_queue_size, len(frontier))
+
+        # using heap to get minimum f(n)
+        _, _, state = heapq.heappop(frontier)
+
+        # remove from dict
+        state_str = str(state.board)
+        if state_str in frontier_dict:
+            del frontier_dict[state_str]
+
+        # check if reach goal
+        if state.whether_is_goal():
+            end_time = time.time()
+            return state, nodes_expanded, max_queue_size, end_time - start_time
+
+        # update explored set
+        explored.add(state_str)
+        nodes_expanded += 1
+
+        # try possible moves
+        for move in state.try_moves():
+            child = state.move(move)
+            child_str = str(child.board)
+
+            if child_str in explored:
+                continue
+
+            child_f = child.cost + heuristic(child)
+
+            # update if find a better path
+            if child_str in frontier_dict:
+                if child_f < frontier_dict[child_str][1]:
+                    # make all old entry invalid
+                    for i, (_, _, s) in enumerate(frontier):
+                        if s == frontier_dict[child_str][0]:
+                            frontier[i] = (float('inf'), counter, s)
+                            heapq.heapify(frontier)
+                            break
+
+                    # add a new entry
+                    heapq.heappush(frontier, (child_f, counter, child))
+                    frontier_dict[child_str] = (child, child_f)
+                    counter += 1
+            else:
+                # add to frontier
+                heapq.heappush(frontier, (child_f, counter, child))
+                frontier_dict[child_str] = (child, child_f)
+                counter += 1
+
+    # no solution
+    end_time = time.time()
+    return None, nodes_expanded, max_queue_size, end_time - start_time
