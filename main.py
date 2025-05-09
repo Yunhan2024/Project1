b47e727b14d12bfed25bee3f4cd3bfcece7b1bbb
SEU-ChangYunhan <yunhan_chang@berkeley.edu>
2025-05-08 21:32:13 -0700

    ucs test and success

--- main.py
+++ main.py
@@ -35,3 +35,17 @@ def main():
         print("Warning: This state is not solvable!")
         print("For the 3x3 eight-puzzle, only half of the initial states are solvable.")
         return
+
+        # 1. uniform cost search
+        print("\nUniform Cost Search:")
+        goal, nodes_expanded, max_queue, time_taken = general_search(initial_state)
+        #general without h(n) is unifrom cost search
+
+        if goal:
+            path = traceback_path(goal)
+            print(f"Solution found! Steps: {len(path) - 1}")
+            print(f"Nodes expanded: {nodes_expanded}")
+            print(f"Maximum queue: {max_queue}")
+            print(f"Time taken: {time_taken * 1000:.2f} ms")
+        else:
+            print("No solution.")
